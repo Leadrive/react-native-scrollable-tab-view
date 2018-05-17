ffed11d4d8cb3081096bef032d45106fa4899bc2
Leadrive <gzakay@163.com>
2018-05-17 22:58:57 +0800

    Update Button.windows.js

--- Button.windows.js
+++ Button.windows.js
@@ -1,14 +1,19 @@
 const React = require('react');
 const ReactNative = require('react-native');
 const {
- TouchableOpacity,
+  TouchableNativeFeedback,
   View,
 } = ReactNative;
 
 const Button = (props) => {
-  return <TouchableOpacity {...props}>
+  return <TouchableNativeFeedback
+    delayPressIn={0}
+    background={TouchableNativeFeedback.SelectableBackground()} // eslint-disable-line new-cap
+    {...props}
+  >
     {props.children}
-  </TouchableOpacity>;
+  </TouchableNativeFeedback>;
 };
 
 module.exports = Button;
+
